3c98ff7095433129f427a2a0af30e505b99c9e9b
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 20:30:45 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,4 +1,17 @@
 require "pry"
 class Meal
   attr_accessor :waiter, :customer, :total, :tip
+  @@all = []
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    save  
+  end
+
+  def save
+    @@all << self 
+  end
+  
 end

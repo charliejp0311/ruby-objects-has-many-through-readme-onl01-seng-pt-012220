8473db166a6fb53935bf39508dc949d924aedb43
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 20:16:47 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,15 @@
 require "pry"
 class Customer
   attr_accessor :name, :age
+  @@all = []
   def initialize(name, age)
     @name = name
     @age = age
+    save
   end
 
+  def save
+    @@all << self 
+  end
 
 end

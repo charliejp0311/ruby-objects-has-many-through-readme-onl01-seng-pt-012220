b72a53a9cec919fc7493c5434ee6096958ffa687
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 20:22:16 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,10 +1,15 @@
 require "pry"
 class Waiter
   attr_accessor :name, :years_of_experience
-
+  @@all = []
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    save 
+  end
+
+  def save 
+    @@all << self     
   end
 
 end
